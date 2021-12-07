ab6283071197e0d5f0c67678ac611c733b6e8815
RuslanKuyanets <kuyanets.exceedteam@gmail.com>
2021-12-07 17:12:19 +0300

    add persist redux for save state

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,5 +1,13 @@
 import { combineReducers, createStore } from 'redux';
 import { todoReducer } from './todo-reducer';
+import { persistStore, persistReducer } from 'redux-persist'
+import localStorage from 'redux-persist/lib/storage' 
+
+
+const persistConfig = {
+    key: 'root',
+    storage: localStorage
+}
 
 export type AppStateType = ReturnType<typeof reducers>
 
@@ -7,6 +15,21 @@ const reducers = combineReducers({
     todo: todoReducer
 })
 
-const store = createStore(reducers)
+
+
+//const store = createStore(reducers) 
+//export default store
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+let store = createStore(persistedReducer)
+export let persistor = persistStore(store)
+
 
 export default store
+
+// export default () => {
+//     let store = createStore(persistedReducer)
+//     let persistor = persistStore(store)
+//     return { store, persistor }
+// }
