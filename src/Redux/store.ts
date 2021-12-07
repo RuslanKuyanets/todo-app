dba3a7c747f99784e3bbbc4660490c5a358e6fe3
RuslanKuyanets <kuyanets.exceedteam@gmail.com>
2021-12-07 17:17:23 +0300

    remove comments from store

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -3,7 +3,6 @@ import { todoReducer } from './todo-reducer';
 import { persistStore, persistReducer } from 'redux-persist'
 import localStorage from 'redux-persist/lib/storage' 
 
-
 const persistConfig = {
     key: 'root',
     storage: localStorage
@@ -15,21 +14,9 @@ const reducers = combineReducers({
     todo: todoReducer
 })
 
-
-
-//const store = createStore(reducers) 
-//export default store
-
 const persistedReducer = persistReducer(persistConfig, reducers)
 
 let store = createStore(persistedReducer)
 export let persistor = persistStore(store)
 
-
 export default store
-
-// export default () => {
-//     let store = createStore(persistedReducer)
-//     let persistor = persistStore(store)
-//     return { store, persistor }
-// }
